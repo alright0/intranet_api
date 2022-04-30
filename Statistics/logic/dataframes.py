0707bfdacfef7a51d76d8254c049112c83f03546
Aleksey <koro6@yandex.ru>
2022-04-30 09:53:11 +0300

    refactor dataframes.py

--- Statistics/logic/dataframes.py
+++ Statistics/logic/dataframes.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import json
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import pandas as pd
 import plotly.graph_objects as go
@@ -26,7 +26,7 @@ class up_puco_table:
     def graph(self):
 
         line_by_shift_list = {}
-        camera_table_df = self._parse_camera()
+        camera_df = self._parse_camera()
 
         for line in self.lines:
 
@@ -36,45 +36,34 @@ class up_puco_table:
                 empty_flag = True  # флаг, указывающий, стоит ли вешать плашку "NO DATA"
 
                 fig = make_subplots()
-                if not camera_table_df.empty and line in IBEA_CAMERA_MAP:
+                if not camera_df.empty and line in IBEA_CAMERA_MAP:
                     for line_side in IBEA_CAMERA_MAP[line]:
 
-                        camera_side_df = camera_table_df.loc[
-                            (camera_table_df["line_side"] == line_side)
-                            & (camera_table_df["shift"] == shift)
+                        camera_side_df = camera_df.loc[
+                            (camera_df["line_side"] == line_side)
+                            & (camera_df["shift"] == shift)
                         ]
 
                         if not camera_side_df.empty:
                             empty_flag = False
 
-                        camera_name_string = (
-                            "{}: {:.2f}% ({} шт. выброшено из {} шт.)".format(
-                                line_side,
-                                camera_side_df["pcs_rejected"].sum() / camera_side_df["pcs_total"].sum() * 100,
-                                camera_side_df["pcs_rejected"].sum(),
-                                camera_side_df["pcs_total"].sum(),
-                            )
-                        )
+                        camera_name_string = self._build_legend_string(camera_side_df, line_side)
 
                         fig.add_trace(
                             go.Scatter(
                                 x=camera_side_df["date_now_sys"],
                                 y=camera_side_df["defect_rate"] * 100,
                                 name=camera_name_string,
-                                hovertemplate=line_side
-                                + "<br>Выброс: %{y:.2f}%.<br>Время: "
-                                + camera_side_df["date_now_sys"].astype(str).str[-8:],
+                                hovertemplate="<extra></extra>"
+                                + "<b>" + line_side
+                                + "</b><br>Выброс: %{y:.2f}%<br>Время: "
+                                + camera_side_df["date_now_sys"].dt.strftime('%H:%M'),
+                                hoverinfo='skip'
                             ),
                         )
+
                     if empty_flag:
-                        fig.add_annotation(
-                            x=0.5,
-                            y=0.5,
-                            xref="paper",
-                            yref="paper",
-                            showarrow=False,
-                            text="NO DATA",
-                        )
+                        self.add_nodata_annotation(fig)
 
                     fig.update_layout(
                         title=f"Line: {line} Shift: {shift}",
@@ -96,7 +85,8 @@ class up_puco_table:
         # запрос под каждую камеру и добавление поля брака
         for line in self.lines:
             if line in IBEA_CAMERA_MAP:
-                df_camera_lvl_0 = pd.DataFrame(list(Camera.get_camera_info(self.date_start, self.date_end, line)))
+                camera_data = Camera.get_camera_info(self.date_start, self.date_end, line)
+                df_camera_lvl_0 = pd.DataFrame(list(camera_data))
                 df_line_list.append(df_camera_lvl_0)
         camera_line_df = camera_line_df.append(df_line_list)
 
@@ -106,17 +96,10 @@ class up_puco_table:
             camera_line_df["defect_rate"] = camera_line_df[["rejected", "total"]].apply(
                 lambda x: x[0]/x[1] if x[1] else 0, axis=1
             )
+            camera_line_df["defect_rate"] = self._cutoff_high_limit(camera_line_df["defect_rate"])
 
             camera_line_df.fillna(0, inplace=True)
-            camera_line_df["shift"] = camera_line_df["date_now_sys"].apply(
-                lambda x: 1
-                if x.hour >= 6 and x.hour < 14
-                else 2
-                if x.hour >= 14 and x.hour < 22
-                else 3
-            )
-            camera_line_df["defect_rate"] = camera_line_df["defect_rate"].apply(
-                lambda x: self.defrate_cutoff if x > self.defrate_cutoff else x)
+            camera_line_df["shift"] = self._map_shifts(camera_line_df["date_now_sys"])
             camera_line_df = camera_line_df.sort_values(by="date_now_sys")
 
             for line_side in set(camera_line_df["line_side"]):
@@ -132,11 +115,74 @@ class up_puco_table:
     # READY: Преобразует графики в json
     @staticmethod
     def graph_to_json(fig):
+        """
+        Экспорт фигуры для интерпретации на фронте в plotly.js.
+
+        :param fig: plotly figure
+        :return: json
+        """
         return json.dumps(fig, cls=PlotlyJSONEncoder)
 
     @staticmethod
-    def cumsum_to_any(final_df_by_line, cumsum_counter):
-        """Конвертирует накопительную сумму в разницу значений накопительной суммы:
-        [10, 20, 25, 30, 45] в [0, 10, 15, 5, 5, 15]"""
+    def cumsum_to_any(df, cumsum_counter):
+        """
+        Конвертирует накопительную сумму в разницу значений накопительной суммы.
+        [10, 20, 25, 30, 45] -> [0, 10, 15, 5, 5, 15].
 
-        return final_df_by_line[cumsum_counter].diff().fillna(0).clip(lower=0).astype(int)
+        :param df: DataFrame
+        :param cumsum_counter: Название столбца по котором будет считаться разница
+        :return: DataFrame
+        """
+
+        return df[cumsum_counter].diff().fillna(0).clip(lower=0).astype(int)
+
+    @staticmethod
+    def _build_legend_string(df, line_side):
+        """
+        Формирует строку для легенды.
+
+        :param df: DataFrame
+        :param line_side: Название стороны камеры
+        :return: Строку с описанием
+        """
+        return "{}: {:.2f}% ({} шт. выброшено из {} шт.)".format(
+                line_side,
+                df["pcs_rejected"].fillna(0).sum() / (df["pcs_total"].sum() if df["pcs_total"].sum() else 1) * 100,
+                df["pcs_rejected"].sum(),
+                df["pcs_total"].sum(),
+            )
+
+    @staticmethod
+    def _map_shifts(df_series):
+        """
+        Размечает series с датами по сменам.
+
+        :param df_series: DataFrame
+        :return: DateFrame
+        """
+        return df_series.apply(
+                lambda x: 1
+                if x.hour >= 6 and x.hour < 14
+                else 2
+                if x.hour >= 14 and x.hour < 22
+                else 3
+        )
+
+    def _cutoff_high_limit(self, df_series):
+        return df_series.apply(lambda x: self.defrate_cutoff if x > self.defrate_cutoff else x)
+
+    @staticmethod
+    def add_nodata_annotation(fig):
+        """
+        Добавляет плашку NO DATA в центре пустого графика.
+
+        :param fig: plotly figure
+        """
+        fig.add_annotation(
+            x=0.5,
+            y=0.5,
+            xref="paper",
+            yref="paper",
+            showarrow=False,
+            text="NO DATA",
+        )
